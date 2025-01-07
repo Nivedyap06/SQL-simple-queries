bfbf1a3ce7c311b3a6afb6e83b8e9e7910f771b7
Nivedyap06 <nivedyapreman12@gmail.com>
2025-01-07 12:28:09 +0530

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,7 @@
+show databases;
+create database abc;
+create database niv;
+use niv;
+create table students(id int primary key, name varchar(20),department varchar(40));
+show tables;
+describe students;
